7f2ba3c5fadda378b4e71c8d6fd11729c60adfa1
Hoyoung Jung <honux77@gmail.com>
2020-11-23 14:48:26 +0900

    Add thread timer animation example

---
+++ game-basic/src/net/honux/game/animation/ThreadBoard.java
@@ -0,0 +1,109 @@
+package net.honux.game.animation;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class ThreadBoard extends JPanel implements Runnable{
+
+    private final int B_WIDTH = 640;
+    private final int B_HEIGHT = 480;
+    private final int INITIAL_X = -40;
+    private final int INITIAL_Y = -40;
+    private final int DELAY = 16;
+
+    private Image star;
+    private Thread animator;
+    private int x, y;
+
+    public ThreadBoard() {
+        initBoard();
+    }
+
+    private void loadImage() {
+        try {
+            star = ImageIO.read(new File("./resources/slime.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private void initBoard() {
+
+        setBackground(Color.BLACK);
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+
+        loadImage();
+
+        x = INITIAL_X;
+        y = INITIAL_Y;
+    }
+
+    @Override
+    public void addNotify() {
+        super.addNotify();
+
+        animator = new Thread(this);
+        animator.start();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        drawStar(g);
+    }
+
+    private void drawStar(Graphics g) {
+
+        g.drawImage(star, x, y, this);
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    private void cycle() {
+
+        x += 1;
+        y += 1;
+
+        if (y > B_HEIGHT) {
+
+            y = INITIAL_Y;
+            x = INITIAL_X;
+        }
+    }
+
+    @Override
+    public void run() {
+
+        long beforeTime, timeDiff, sleep;
+
+        beforeTime = System.currentTimeMillis();
+
+        while (true) {
+
+            cycle();
+            repaint();
+
+            timeDiff = System.currentTimeMillis() - beforeTime;
+            sleep = DELAY - timeDiff;
+
+            if (sleep < 0) {
+                sleep = 2;
+            }
+
+            try {
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+
+                String msg = String.format("Thread interrupted: %s", e.getMessage());
+
+                JOptionPane.showMessageDialog(this, msg, "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+
+            beforeTime = System.currentTimeMillis();
+        }
+    }
+}
